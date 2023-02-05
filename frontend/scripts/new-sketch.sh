608ea3f565c0ed79493cf5a427cb7f041be59122
David Holcer <davidholcer@gmail.com>
2023-02-04 20:28:59 -0500

    up-to-date

--- frontend/scripts/new-sketch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-FILECOUNT=$(find ./sketches/ -type f | wc -l | tr -d ' ')
-cp ./sketches/dN.js "./sketches/d${FILECOUNT}.js"
-echo "d${FILECOUNT}.js is created."
--- frontend/utils/utils.js
+++
@@ -1,4 +0,0 @@
-const range = (n) => Array.from({length: n}, (v, k) => k);  
-export { 
-  range 
-}
